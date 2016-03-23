acf55a77cac726c7a7432e1d11a2adf03bfa168e
lpoujade <lpoujade@student.42.fr>
2016-03-23 20:54:07 +0100

    - see libft commit

--- src/lsfile.c
+++ src/lsfile.c
@@ -6,13 +6,13 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 14:08:04 by lpoujade          #+#    #+#             */
-/*   Updated: 2016/03/23 04:15:26 by lpoujade         ###   ########.fr       */
+/*   Updated: 2016/03/23 20:53:43 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void		eval(t_fileinfo **fflist, t_params opts, int c)
+void			eval(t_fileinfo **fflist, t_params opts, int c)
 {
 	t_fileinfo		*tmp;
 	struct stat		stated_file;
@@ -23,11 +23,13 @@ void		eval(t_fileinfo **fflist, t_params opts, int c)
 		if (tmp->tot && tmp->prev)
 		{
 			ft_putchar('\n');
-			ft_putstr(tmp->infos); ft_putendl(":");
-			if (opts&LONG_FORMAT)
+			ft_putstr(tmp->infos);
+			ft_putendl(":");
+			if (opts & LONG_FORMAT)
 			{
 				ft_putstr("total ");
-				ft_putnbr(tmp->tot); ft_putchar('\n');
+				ft_putnbr(tmp->tot);
+				ft_putchar('\n');
 			}
 		}
 		else
@@ -38,11 +40,8 @@ void		eval(t_fileinfo **fflist, t_params opts, int c)
 				exit(3);
 			}
 			if (stated_file.st_mode & S_IFDIR && (opts & 0x04 || c > 0))
-			{
-				ft_lstappend((t_list*)*fflist, (t_list*)fold_list(tmp->infos, opts));
-				//ft_lstinsert((t_list**)fflist, (t_list*)fold_list(tmp->infos, opts), &fts_strcmp);
-			}
-			if (c > 0 || !(opts & ONLY_FOLD))
+				ft_lstinsert_list((t_list*)*fflist, (t_list*)fold_list(tmp->infos, opts), &fts_strcmp);
+			if ((c > 0 || !(opts & ONLY_FOLD)) && tmp->tot!= -1)
 				print_file_infos(stated_file, tmp->infos, opts);
 			((tmp && !tmp->next) && !(opts & 0x01)) ? ft_putchar('\n') : 0;
 			ft_bzero((void**)&stated_file, sizeof(struct stat));
@@ -62,7 +61,8 @@ t_fileinfo		*fold_list(char *dname, t_params opts)
 	fflist = NULL;
 	if ((ddir = opendir(dname)))
 	{
-		fflist = (t_fileinfo*)fts_new(dname); tot = &(*fflist).tot;
+		fflist = (t_fileinfo*)fts_new(dname);
+		tot = &(*fflist).tot;
 		while ((dfile = readdir(ddir)))
 			if (*dfile->d_name != '.' || opts & 0x08 ||
 					(opts & 0x20 && (ft_strcmp(dfile->d_name, ".")
@@ -73,7 +73,7 @@ t_fileinfo		*fold_list(char *dname, t_params opts)
 				(*tot)++;
 			}
 		if (!*tot)
-			*tot = 1;
+			*tot = -1;
 		if ((closedir(ddir) != 0))
 			perror("ls: ");
 	}
